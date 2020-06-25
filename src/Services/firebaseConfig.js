0a5e146ea6d2511dc16a2b15d380c56cc361f6da
Srihari <Srhrsrihari@gmail.com>
2020-06-25 17:53:05 +0530

    fix projectId final

--- src/Services/firebaseConfig.js
+++ src/Services/firebaseConfig.js
@@ -4,7 +4,7 @@ import "@firebase/firestore";
 const config =  firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID.toString()
+    projectId: 'mydform'
   });
 
 
